44cdea1d37919c74fa9eb69d5de3d3988be0d2b3
Chanwoo <tree698@gmail.com>
2022-03-12 17:30:00 +0900

    Apply Function Component on ItemAddForm.jsx

--- src/component/itemAddForm.jsx
+++ src/component/itemAddForm.jsx
@@ -1,27 +1,26 @@
-import React, { PureComponent } from 'react';
+import React, { memo } from 'react';
 
-class ItemAddForm extends PureComponent {
-  inputRef = React.createRef();
+const ItemAddForm = memo((props) => {
+  const inputRef = React.createRef();
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onSubmit(name);
-    this.inputRef.current.value = '';
+    const name = inputRef.current.value;
+    name && props.onSubmit(name);
+    inputRef.current.value = '';
   };
-  render() {
-    return (
-      <form className="add-form" onSubmit={this.handleSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Enter your item"
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Enter your item"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default ItemAddForm;
